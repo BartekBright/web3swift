57881c168bd3a2c023367a35330c9f4c4bfa5390
Bartek <bartek.rozwarski@brightinventions.pl>
2020-09-17 09:44:13 +0200

    update

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Web3swift",
   platforms: [
-      .iOS(.v10),
-      .macOS(.v10_11),
-      .tvOS(.v10)
+    .iOS(SupportedPlatform.IOSVersion.v9)
   ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
